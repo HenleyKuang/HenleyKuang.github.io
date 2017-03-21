f7c0a6ed39f384fd6d982478eec0d99d43468cc0
Henley <hekuang@ucdavis.edu>
2017-03-21 00:21:56 -0700

    Original Source

---
+++ js/index.js
@@ -0,0 +1,18 @@
+angular.module('nbaStatsApp', [])
+  .controller('appController', function( $scope ) {
+    var appCtrls = this;
+ 
+    appCtrls.getScores = function( ) {
+      	var url = `http://stats.nba.com/stats/scoreboard/?
+				GameDate=` + appCtrls.date + `&
+				LeagueID=00&
+				DayOffset=00&
+				callback=?`;  
+				
+		$.getJSON (url, function (json) {
+			$scope.$apply(function () {  
+				appCtrls.scoreBoard = json;
+			});
+		  });
+    };
+  });
