aee44d0d2c9384bc86163053d49d0cfd29ee47ba
Henley <hekuang@ucdavis.edu>
2017-03-21 01:03:49 -0700

    fixed http request and added font awesome

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 angular.module('nbaStatsApp', [])
   .controller('appController', function( $scope ) {
     var appCtrls = this;
+	appCtrls.loading = false;
  
     appCtrls.getScores = function( ) {
-      	var url = `http://stats.nba.com/stats/scoreboard/?
+		appCtrls.loading = true;
+      	var url = `//stats.nba.com/stats/scoreboard/?
 				GameDate=` + appCtrls.date + `&
 				LeagueID=00&
 				DayOffset=00&
@@ -13,6 +15,10 @@ angular.module('nbaStatsApp', [])
 			$scope.$apply(function () {  
 				appCtrls.scoreBoard = json;
 			});
-		  });
+		  }).then(function () {      
+			$scope.$apply(function () {
+				appCtrls.loading = false;
+			});
+		});
     };
   });
