e10ef46f4b04cba79cf45d05c8817d555d83970d
Henley <hekuang@ucdavis.edu>
2017-03-21 01:06:39 -0700

    remove loading icon

--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
 angular.module('nbaStatsApp', [])
   .controller('appController', function( $scope ) {
     var appCtrls = this;
-	appCtrls.loading = false;
  
     appCtrls.getScores = function( ) {
-		appCtrls.loading = true;
-      	var url = `//stats.nba.com/stats/scoreboard/?
+      	var url = `http://stats.nba.com/stats/scoreboard/?
 				GameDate=` + appCtrls.date + `&
 				LeagueID=00&
 				DayOffset=00&
@@ -15,10 +13,6 @@ angular.module('nbaStatsApp', [])
 			$scope.$apply(function () {  
 				appCtrls.scoreBoard = json;
 			});
-		  }).then(function () {      
-			$scope.$apply(function () {
-				appCtrls.loading = false;
-			});
-		});
+		  });
     };
   });
