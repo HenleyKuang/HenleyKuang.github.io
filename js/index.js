29b327756efc247909161de59ae2547dddc39ce0
Henley Kuang <hekuang@ucdavis.edu>
2017-03-21 16:31:00 -0700

    added fullschedule json

--- js/index.js
+++ js/index.js
@@ -1,18 +1,23 @@
 angular.module('nbaStatsApp', [])
   .controller('appController', function( $scope ) {
     var appCtrls = this;
- 
+
+    appCtrls.apiEndPoints = {
+      boxScore: 'http://stats.nba.com/stats/boxscoreadvancedv2/?StartPeriod=0&EndPeriod=0&StartRange=0&EndRange=0&RangeType=0&GameId=0021600896',
+      scoreBoard: 'http://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2016/league/00_full_schedule.json'
+    }
+
+    appCtrls.getJSON = function ( url, successCallback ) {
+      $.getJSON (url, function (json) {
+    			$scope.$apply(function () {
+    				successCallback();
+    			});
+  		  });
+    }
+
     appCtrls.getScores = function( ) {
-      	var url = `http://stats.nba.com/stats/scoreboard/?
-				GameDate=` + appCtrls.date + `&
-				LeagueID=00&
-				DayOffset=00&
-				callback=?`;  
-				
-		$.getJSON (url, function (json) {
-			$scope.$apply(function () {  
-				appCtrls.scoreBoard = json;
-			});
-		  });
+      	var url = appCtrls.apiEndPoints.scoreBoard;
+
+        appCtrls.getJSON( appCtrls.apiEndPoints.scoreBoard, function () {appCtrls.scoreBoard = json} );
     };
   });
